635e1f4a2f804c4c08745c526baeed8b8f81a130
Daniel Burgess <dburgess@silhouettesoftware.com>
2019-03-27 16:41:55 +0000

    Fixed some of the used formulas
    Some of the formulas were slightly broken, fixed them

--- Python Playground/CSV Editor/Formula Editor.py
+++ Python Playground/CSV Editor/Formula Editor.py
@@ -8,13 +8,13 @@ def levelTestFormula():
 
     for row in csvFile:
         count = count+1
-        if row[1] == "Pass" or row[1] == "Fail" or row[1] == "N/A" or row[1] == "To Test":
-            row[1] = '=IFS(INDIRECT("Setup!B{}")=True,"Pass",INDIRECT("Setup!C{}")=True,"Fail",INDIRECT("Setup!D{}")=True,"N/A",INDIRECT("Setup!E{}")=True,"Retest")'.format(count+2,count+2,count+2,count+2)
+        if row[1] == "Pass" or row[1] == "Fail" or row[1] == "N/A" or row[1] == "To Test" or row[1] == "Retest" or row[1] == "#N/A":
+            row[1] = '=IFS(INDIRECT("Setup!B{}")=True,"Pass",INDIRECT("Setup!C{}")=True,"Fail",INDIRECT("Setup!D{}")=True,"N/A",INDIRECT("Setup!E{}")=True,"Retest")'.format(count,count,count,count)
     count=0
     for row in csvFile:
         count = count+1
-        if row[2] == "Pass" or row[2] == "Fail" or row[2] == "N/A" or row[2] == "To Test":
-            row[2] = '=IFS(INDIRECT("Setup!F{}")=True,"Pass",INDIRECT("Setup!G{}")=True,"Fail",INDIRECT("Setup!H{}")=True,"N/A",INDIRECT("Setup!I{}")=True,"Retest")'.format(count+2,count+2,count+2,count+2)
+        if row[2] == "Pass" or row[2] == "Fail" or row[2] == "N/A" or row[2] == "To Test" or row[2] == "Retest" or row[2] == "#N/A":
+            row[2] = '=IFS(INDIRECT("Setup!F{}")=True,"Pass",INDIRECT("Setup!G{}")=True,"Fail",INDIRECT("Setup!H{}")=True,"N/A",INDIRECT("Setup!I{}")=True,"Retest")'.format(count,count,count,count)
 
     with open("C:\\Users\\dburgess\\Documents\\GitHub\\Python Playground\\Python Playground\\CSV Editor\\UpdatedFormula.csv", "w") as output:
         for row in tqdm(csvFile):
@@ -33,11 +33,11 @@ def setupFormula():
             row[0] = '=ISNUMBER(FIND("Success",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
             row[1] = '=ISNUMBER(FIND("Fail",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
             row[2] = '=ISNUMBER(FIND("N/A",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
-            row[4] = "=NOT(OR(C{}=True,B{}=True,A{}=True))".format(count, count, count)
+            row[3] = "=NOT(OR(C{}=True,B{}=True,A{}=True))".format(count, count, count)
             #Mac
-            row[4] = '=ISNUMBER(FIND("Success",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
-            row[5] = '=ISNUMBER(FIND("Fail",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
-            row[6] = '=ISNUMBER(FIND("N/A",INDIRECT("'+"'Level Test'!G{}".format(count)+'")))'
+            row[4] = '=ISNUMBER(FIND("Success",INDIRECT("'+"'Level Test'!H{}".format(count)+'")))'
+            row[5] = '=ISNUMBER(FIND("Fail",INDIRECT("'+"'Level Test'!H{}".format(count)+'")))'
+            row[6] = '=ISNUMBER(FIND("N/A",INDIRECT("'+"'Level Test'!H{}".format(count)+'")))'
             row[7] = "=NOT(OR(E{}=True,F{}=True,G{}=True))".format(count, count, count)
 
     with open("C:\\Users\\dburgess\\Documents\\GitHub\\Python Playground\\Python Playground\\CSV Editor\\UpdatedSetup.csv", "w") as output:
