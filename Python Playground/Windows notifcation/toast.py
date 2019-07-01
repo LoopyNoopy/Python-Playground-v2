9efacf021394791beb22682f6efc23d45de42775
Daniel Burgess <dburgess@silhouettesoftware.com>
2019-07-01 16:11:32 +0100

    Added toast
    Added toast.py - Lets you create windows notifications
    
    Slight edit to the editCSV function

---
+++ Python Playground/Windows notifcation/toast.py
@@ -0,0 +1,4 @@
+from win10toast import ToastNotifier
+toaster = ToastNotifier()
+toaster.show_toast("Auto Test - Activate Silhouette Studio","Test has finished, begining next test", duration = 3)
+toaster.show_toast("Auto Test - Dectivate Silhouette Studio","Test has finished, begining next test", duration = 3)
