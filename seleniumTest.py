110c71e7c9be4124c2dc4347e10f99db07326e67
Daniel Burgess <danieljbburgess@hotmail.co.uk>
2023-02-17 11:58:15 +0000

    Randomness

--- seleniumTest.py
+++ seleniumTest.py
@@ -3,13 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-browser = webdriver.Chrome()
 
-browser.get('http://www.yahoo.com')
-time.sleep(5) # Let the user actually see something!
-assert 'Yahoo' in browser.title
+def test_confirm_yahoo_title():
+    browser = webdriver.Chrome()
 
-elem = browser.find_element(By.NAME, 'p')  # Find the search box
-elem.send_keys('seleniumhq' + Keys.RETURN)
+    browser.get('http://www.yahoo.com')
+    time.sleep(3) # Let the user actually see something!
+    browser.find_element(By.NAME,"agree").click()
+    time.sleep(3) # Let the user actually see something!
+    assert 'Yahoo' in browser.title
 
-browser.quit()
+    elem = browser.find_element(By.NAME, 'p')  # Find the search box
+    elem.send_keys('Silhouette America' + Keys.RETURN)
+
+    time.sleep(5) # Let the user actually see something!
+
+    browser.quit()
